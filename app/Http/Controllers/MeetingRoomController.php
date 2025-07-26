ca10bcb1b8070206370ba277ba0c1cfebc895a3c
WidyaX1900 <tuderangga93@gmail,com>
2025-07-26 19:02:39 +0800

    setup: model, controller, migration, migration and seeder for meeting room data

---
+++ app/Http/Controllers/MeetingRoomController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MeetingRoom;
+use App\Http\Requests\StoreMeetingRoomRequest;
+use App\Http\Requests\UpdateMeetingRoomRequest;
+
+class MeetingRoomController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreMeetingRoomRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(MeetingRoom $meetingRoom)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(MeetingRoom $meetingRoom)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateMeetingRoomRequest $request, MeetingRoom $meetingRoom)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(MeetingRoom $meetingRoom)
+    {
+        //
+    }
+}
