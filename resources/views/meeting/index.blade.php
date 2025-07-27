b29624af85b4f5a8db5f65c3d4c65e26955ad86b
WidyaX1900 <tuderangga93@gmail,com>
2025-07-27 13:06:52 +0800

    modified: styling meeting room

--- resources/views/meeting/index.blade.php
+++ resources/views/meeting/index.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts.layout')
 @section('content')
-    <div class="container meeting-container"></div>
+    <div id="meetingRoom" class="meeting-container">
+        <div class="full-video-container">
+            <video id="fullVideo" muted autoplay playsinline></video>
+            <div class="participant-name">
+                <h5 id="participantName" class="text-light ms-3"></h5>
+            </div>
+        </div>
+        <div class="list-video-container">
+            <div class="list-video">
+                <video id="localVideo" muted autoplay playsinline></video>
+            </div>
+        </div>
+    </div>
 @endsection
