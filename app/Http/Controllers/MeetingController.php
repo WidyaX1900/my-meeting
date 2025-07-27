577bd775395a1f76c9a9926d8d8f00712ebba175
WidyaX1900 <tuderangga93@gmail,com>
2025-07-27 12:37:24 +0800

    view: meeting room

--- app/Http/Controllers/MeetingController.php
+++ app/Http/Controllers/MeetingController.php
@@ -12,7 +12,7 @@ class MeetingController extends Controller
     public function index()
     {
         $room_token = session('room_token');
-        echo "Room meeting {$room_token}";
+        return view('meeting_room.room', ['room_token' => $room_token]);
     }
     
     public function join(MeetingRoom $meeting_room, $room_token = null)
---
+++ resources/views/meeting_room/room.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.layout')
+@section('content')
+    <div class="container meeting-container"></div>
+@endsection
