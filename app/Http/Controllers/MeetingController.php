9d1ce8d96b185df2e7503403a361251e57c4c9b7
WidyaX1900 <tuderangga93@gmail,com>
2025-07-27 12:39:19 +0800

    fix: meeting room view folder structure

--- app/Http/Controllers/MeetingController.php
+++ app/Http/Controllers/MeetingController.php
@@ -12,7 +12,7 @@ class MeetingController extends Controller
     public function index()
     {
         $room_token = session('room_token');
-        return view('meeting_room.room', ['room_token' => $room_token]);
+        return view('meeting.index', ['room_token' => $room_token]);
     }
     
     public function join(MeetingRoom $meeting_room, $room_token = null)
