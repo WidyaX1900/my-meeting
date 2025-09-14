01361f84ec7a9d40e953b6d64eab4df7d7dc9303
WidyaX1900 <tuderangga93@gmail,com>
2025-09-14 10:14:09 +0800

    refactor: static asset position for user's page

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -7,9 +7,14 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Simple Video Conference App</title>
-    <link rel="stylesheet" href="{{ config('app.asset_url') }}bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
-    @vite(['resources/js/app.js', 'resources/js/script.js', 'resources/js/meeting.js', 'resources/css/style.css'])
+    @vite([
+        'resources/js/app.js', 
+        'resources/js/script.js', 
+        'resources/js/meeting.js', 
+        'resources/css/style.css',
+        'resources/bootstrap/css/bootstrap.min.css'
+    ])
 </head>
 
 <body>
@@ -17,7 +22,7 @@
     @yield('content')
     @yield('modal')
     <script src="https://cdn.socket.io/4.8.1/socket.io.min.js"></script>
-    <script src="{{ config('app.asset_url') }}js/jquery-3.7.1.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
         integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
     </script>
